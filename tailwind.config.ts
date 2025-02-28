ffe88567fb05763adb48c86bc58fe95205704782
Dung Tran <admin@192.168.1.6.non-exists.ptr.local>
2025-02-28 21:50:52 +0700

    main: add config color, fontfamily

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,47 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+import type { Config } from 'tailwindcss';
+import { COLOR } from './src/app/styles/color';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        brand: COLOR.brand['500'],
+        brandPrimaryDark: COLOR.brand['600'],
+        brandPrimary: COLOR.brand['500'],
+        brandPrimaryLight: COLOR.brand['300'],
+        primaryOnLight: COLOR.grayscale['900'],
+        secondaryOnLight: COLOR.grayscale['700'],
+        tertiaryOnDark: COLOR.grayscale['600'],
+        tertiaryOnLight: COLOR.grayscale['300'],
+        primaryOnDark: COLOR.grayscale.white,
+        secondaryOnDark: COLOR.grayscale['200'],
+        border: {
+          tertiaryOnDark: COLOR.grayscale['500'],
+          tertiaryOnLight: COLOR.grayscale['400'],
+        },
+        text: {
+          tertiaryOnDark: COLOR.grayscale['500'],
+          tertiaryOnLight: COLOR.grayscale['400'],
+        },
       },
+    },
+    screens: {
+      xs: '420px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    fontFamily: {
+      sans: ['Plus Jakarta Sans Variable', ...fontFamily.sans],
     },
   },
   plugins: [],
